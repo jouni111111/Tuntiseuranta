19cf6a973e7975ac1b8a3a890ba1037ee402ac80
keke5000 <keke5000@gmail.com>
2018-07-05 11:10:22 +0300

    heheh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import sun.net.ConnectionResetException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Kantakasittelija kantakasittelija = new Kantakasittelija();
+        kantakasittelija.yhteydenLuonti();
+        Connection con = kantakasittelija.getCon();
 
         Kayttoliittyma kayttis = new Kayttoliittyma();
-        kayttis.start(kantakasittelija.getCon());
+        kayttis.start(con);
 
     }
 }
