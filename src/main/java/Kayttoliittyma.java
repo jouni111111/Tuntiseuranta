cc0121fa30290d51f8f37f0c77bc586b3041e26d
keke5000 <keke5000@gmail.com>
2018-07-05 11:33:36 +0300

    kayttista tehty

--- src/main/java/Kayttoliittyma.java
+++ src/main/java/Kayttoliittyma.java
@@ -12,7 +12,7 @@ public class Kayttoliittyma {
         System.out.println();
         System.out.println();
 
-        while(true){
+        while (true) {
 
             System.out.println("Tervetuloa kirjaamaan tuntisi, valitse alla olevista vaihtoehdoista. ");
             System.out.println();
@@ -20,10 +20,40 @@ public class Kayttoliittyma {
             System.out.println("2. Kirjaa tunnit");
             System.out.println("3. Raportti");
 
+            int syote = Integer.parseInt(lukija.nextLine());
+
+            //TODO tee switch case tästä
+            if (syote == 1) {
+                teeUusiKayttaja(lukija);
+            }
+
+
             break;
         }
 
 
+    }
 
+    private void teeUusiKayttaja(Scanner lukija) {
+
+        System.out.println("Seuravaaksi luodaan uusi käyttäjä. Seuraa ohjeita ja kirjoita vaadittavat tiedot. Pakolliset kentät on merkitty * merkillä.\n");
+        System.out.print("Etunimi*: ");
+        String etunimi = lukija.nextLine();
+
+        System.out.print("Sukunimi*: ");
+        String sukunimi = lukija.nextLine();
+
+        System.out.println("Osasto: ");
+        String osasto = lukija.nextLine();
+
+        System.out.println("Tehtavanimike: ");
+        String tehtavanimike = lukija.nextLine();
+
+        Kayttaja kayttaja = new Kayttaja(etunimi, sukunimi, osasto, tehtavanimike);
+
+        /*
+
+
+         */
     }
 }
