94be6640c62fa67c67c313c6ecda6275d92befd1
keke5000 <keke5000@gmail.com>
2018-07-05 11:02:06 +0300

    käyttis ja main

--- src/main/java/Kayttoliittyma.java
+++ src/main/java/Kayttoliittyma.java
@@ -1,2 +1,29 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Kayttoliittyma {
+
+    public void start(Connection con) throws SQLException {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Tuntari - Tuntikirjausten Uber");
+        System.out.println("===============================");
+        System.out.println();
+        System.out.println();
+
+        while(true){
+
+            System.out.println("Tervetuloa kirjaamaan tuntisi, valitse alla olevista vaihtoehdoista. ");
+            System.out.println();
+            System.out.println("1. Rekisteröi käyttäjä");
+            System.out.println("2. Kirjaa tunnit");
+            System.out.println("3. Raportti");
+
+            break;
+        }
+
+
+
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Kantakasittelija kantakasittelija = new Kantakasittelija();
+
+        Kayttoliittyma kayttis = new Kayttoliittyma();
+        kayttis.start(kantakasittelija.getCon());
     }
 }
